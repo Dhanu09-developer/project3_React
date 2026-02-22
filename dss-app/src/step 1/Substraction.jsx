c419f2d02e547b5790f34006dd3f07c6cb414d55
Dhanashree <dhanashreehodage45@gmail.com>
2026-02-22 11:50:27 +0530

    Done with Sub

---
+++ dss-app/src/step 1/Substraction.jsx
@@ -0,0 +1,13 @@
+
+
+let Substraction = (a, b) => {
+    return (
+        <div>
+            <p>Substraction</p>
+            <p>{a - b}</p>
+
+        </div>
+    )
+}
+
+export default Substraction;
